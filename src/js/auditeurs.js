e58c32fc1775cc085c4c6e0789b0f9f9f8c31036
Linda Benrighi <Linda Benrighi>
2017-04-24 15:02:05 +0200

    Pages auditeurs et juniors

--- src/js/auditeurs.js
+++ src/js/auditeurs.js
@@ -1,78 +1,90 @@
-var myLineChart = new Chart(ctx).Line(data, option);
-var data = {
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
-    datasets: [
-        {
-            label: "My First dataset",
-            fillColor: "rgba(220,220,220,0.2)",
-            strokeColor: "rgba(220,220,220,1)",
-            pointColor: "rgba(220,220,220,1)",
-            pointStrokeColor: "#fff",
-            pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(220,220,220,1)",
-            data: [65, 59, 80, 81, 56, 55, 40]
-        },
-        {
-            label: "My Second dataset",
-            fillColor: "rgba(151,187,205,0.2)",
-            strokeColor: "rgba(151,187,205,1)",
-            pointColor: "rgba(151,187,205,1)",
-            pointStrokeColor: "#fff",
-            pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(151,187,205,1)",
-            data: [28, 48, 40, 19, 86, 27, 90]
-        }
-    ]
-};
 
 $(function () {
-    // Get the context of the canvas element we want to select
-    var ctx = document.getElementById("myChart").getContext('2d');
-    var myLineChart = new Chart(ctx).Line(data, option); //'Line' defines type of the chart.
+    var ctx = document.getElementById("chartOrga");
+	var myChart = new Chart(ctx, {
+		type: 'bar',
+		data: {
+			labels: ["Gestion Asso", "SMQ", "Dev Co", "Suivi d'Etude", "Recrutement", "Formation"],
+			datasets: [{
+				label: 'Note',
+				data: [4, 3, 3, 5, 2, 3],
+				backgroundColor: [
+					'rgba(255, 99, 132, 0.2)',
+					'rgba(54, 162, 235, 0.2)',
+					'rgba(255, 206, 86, 0.2)',
+					'rgba(75, 192, 192, 0.2)',
+					'rgba(153, 102, 255, 0.2)',
+					'rgba(255, 159, 64, 0.2)'
+				],
+				borderColor: [
+					'rgba(255,99,132,1)',
+					'rgba(54, 162, 235, 1)',
+					'rgba(255, 206, 86, 1)',
+					'rgba(75, 192, 192, 1)',
+					'rgba(153, 102, 255, 1)',
+					'rgba(255, 159, 64, 1)'
+				],
+				borderWidth: 1
+			}]
+		},
+		options: {
+			scales: {
+				yAxes: [{
+					ticks: {
+						beginAtZero:true
+					}
+				}]
+			}
+		}
+	});
+   
+    var option = {
+		responsive: true,
+    };
+
 });
 
 $(function () {
-    var option = {
-    responsive: true,
-    };
+    var ctx = document.getElementById("chartTreso");
+	var myChart = new Chart(ctx, {
+		type: 'bar',
+		data: {
+			labels: ["Budget", "Compta", "Social", "Fiscal", "Ventes", "Achat"],
+			datasets: [{
+				label: 'Note',
+				data: [2, 2, 3, 4, 3, 5],
+				backgroundColor: [
+					'rgba(255, 99, 132, 0.2)',
+					'rgba(54, 162, 235, 0.2)',
+					'rgba(255, 206, 86, 0.2)',
+					'rgba(75, 192, 192, 0.2)',
+					'rgba(153, 102, 255, 0.2)',
+					'rgba(255, 159, 64, 0.2)'
+				],
+				borderColor: [
+					'rgba(255,99,132,1)',
+					'rgba(54, 162, 235, 1)',
+					'rgba(255, 206, 86, 1)',
+					'rgba(75, 192, 192, 1)',
+					'rgba(153, 102, 255, 1)',
+					'rgba(255, 159, 64, 1)'
+				],
+				borderWidth: 1
+			}]
+		},
+		options: {
+			scales: {
+				yAxes: [{
+					ticks: {
+						beginAtZero:true
+					}
+				}]
+			}
+		}
+	});
    
-    // Get the context of the canvas element we want to select
-    var ctx = document.getElementById("myChart").getContext('2d');
-    var myLineChart = new Chart(ctx).Line(data, option); //'Line' defines type of the chart.
-});
+    var option = {
+		responsive: true,
+    };
 
-$(function () {
-    var data = {
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
-    datasets: [
-        {
-            label: "My First dataset",
-            fillColor: "rgba(220,220,220,0.2)",
-            strokeColor: "rgba(220,220,220,1)",
-            pointColor: "rgba(220,220,220,1)",
-            pointStrokeColor: "#fff",
-            pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(220,220,220,1)",
-            data: [65, 59, 80, 81, 56, 55, 40]
-        },
-        {
-            label: "My Second dataset",
-            fillColor: "rgba(151,187,205,0.2)",
-            strokeColor: "rgba(151,187,205,1)",
-            pointColor: "rgba(151,187,205,1)",
-            pointStrokeColor: "#fff",
-            pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(151,187,205,1)",
-            data: [28, 48, 40, 19, 86, 27, 90]
-        }
-        ]
-    };
-   
-    var option = {
-    responsive: true,
-    };
-   
-    // Get the context of the canvas element we want to select
-    var ctx = document.getElementById("myChart").getContext('2d');
-    var myLineChart = new Chart(ctx).Line(data, option); //'Line' defines type of the chart.
 });
---
+++ src/js/junior.js
@@ -0,0 +1,88 @@
+
+$(function () {
+    var ctx = document.getElementById("chartOrga");
+	var myChart = new Chart(ctx, {
+		type: 'radar',
+		data: {
+			labels: ["Gestion Asso", "SMQ", "Dev Co", "Suivi d'Etude", "Recrutement", "Formation"],
+			datasets: [{
+				label: 'Profil Orga',
+				data: [4, 3, 3, 5, 2, 3],
+				backgroundColor: [
+					'rgba(255, 99, 132, 0.2)',
+					'rgba(54, 162, 235, 0.2)',
+					'rgba(255, 206, 86, 0.2)',
+					'rgba(75, 192, 192, 0.2)',
+					'rgba(153, 102, 255, 0.2)',
+					'rgba(255, 159, 64, 0.2)'
+				],
+				borderColor: [
+					'rgba(255,99,132,1)',
+					'rgba(54, 162, 235, 1)',
+					'rgba(255, 206, 86, 1)',
+					'rgba(75, 192, 192, 1)',
+					'rgba(153, 102, 255, 1)',
+					'rgba(255, 159, 64, 1)'
+				],
+				borderWidth: 1
+			}]
+		},
+		options: {
+			scale: {
+				reverse: false,
+				ticks: {
+					beginAtZero: true
+				}
+			}
+		}
+	});
+   
+    var option = {
+		responsive: true,
+    };
+
+});
+
+$(function () {
+    var ctx = document.getElementById("chartTreso");
+	var myChart = new Chart(ctx, {
+		type: 'radar',
+		data: {
+			labels: ["Budget", "Compta", "Social", "Fiscal", "Ventes", "Achat"],
+			datasets: [{
+				label: 'Profil tréso',
+				data: [2, 2, 3, 4, 3, 5],
+				backgroundColor: [
+					'rgba(255, 99, 132, 0.2)',
+					'rgba(54, 162, 235, 0.2)',
+					'rgba(255, 206, 86, 0.2)',
+					'rgba(75, 192, 192, 0.2)',
+					'rgba(153, 102, 255, 0.2)',
+					'rgba(255, 159, 64, 0.2)'
+				],
+				borderColor: [
+					'rgba(255,99,132,1)',
+					'rgba(54, 162, 235, 1)',
+					'rgba(255, 206, 86, 1)',
+					'rgba(75, 192, 192, 1)',
+					'rgba(153, 102, 255, 1)',
+					'rgba(255, 159, 64, 1)'
+				],
+				borderWidth: 1
+			}]
+		},
+		options: {
+			scale: {
+				reverse: false,
+				ticks: {
+					beginAtZero: true
+				}
+			}
+		}
+	});
+   
+    var option = {
+		responsive: true,
+    };
+
+});
